853c8b556975981b69b3df70165e43527f27140d
Juan Ospina <jeol3739@gmail.com>
2023-05-18 12:45:10 -0500

    Ejercicio 1 Java Intro

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,14 @@
+
+package ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        String nombre;
+        int num1;
+        byte num2;
+        char letra;
+        float num3;
+        double num4;        
+    }    
+}
